e0933ddfd9292a9bbbff7073684961b606e39eb9
joisemp <joise1124@gmail.com>
2024-12-09 10:29:05 +0530

    inital commit

---
+++ maths_problem/Math.java
@@ -0,0 +1,12 @@
+public class Math {
+    public static void main(String args[]){
+        int num1 = 10;
+        int num2 = 5;
+        int sum = num1+num2;
+        int mul = num1*num2;
+        double div = num1/num2;
+        System.out.println("The sum of "+ num1 + " and " + num2 + " is " + sum);
+        System.out.println("The product of "+ num1 + " and " + num2 + " is " + mul);
+        System.out.println("The result of "+ num1 + " / " + num2 + " is " + div);
+    }
+}
---
+++ strings_java/StingOperations.java
@@ -0,0 +1,6 @@
+public class StingOperations {
+    public static void main(String[] args) {
+        String hello = "hello world";
+        System.out.println(hello);
+    }
+}
